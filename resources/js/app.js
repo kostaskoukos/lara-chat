d6671cc7a6767fd5625d001b1f52533099c1d448
Kostas Koukos <kkonst2014@gmail.com>
2023-08-31 15:31:49 +0300

    bumped deps & added alpine-ajax

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 // import './bootstrap';
 import Alpine from "alpinejs";
 import jsz from "alpinejs-jsz";
+import ajax from "@imacrayon/alpine-ajax";
 
 window.Alpine = Alpine;
 Alpine.plugin(jsz);
+Alpine.plugin(ajax);
 Alpine.start();
