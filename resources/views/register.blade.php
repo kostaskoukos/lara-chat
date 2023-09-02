fb243fec6a47cd05d904cb99505202876c2bb02c
Kostas Koukos <kkonst2014@gmail.com>
2023-09-02 16:02:44 +0300

    fixed google-auth from bug finished regsiter page styling

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,11 +1,20 @@
 <x-layout title="Register">
-    <form action="/register" method="POST">
-        @csrf
-        Name<input required type="text" name="name">
-        Email<input required type="email" name="email">
-        Password<input required type="password" name="password">
-        Password Confirm<input required type="password" name="password_confirmation">
-        <button type="submit">Register</button>
-        <x-googleauth />
-    </form>
+    <div class="flex items-center h-screen">
+        <form action="/register" method="POST" class="card m-auto w-screen md:w-1/3 md:bg-base-200">
+
+            <div class="card-body">
+                <h1 class="card-title">Register</h1>
+                @csrf
+                <label for="name" class="label">Name</label><input class="input input-bordered" required type="text" name="name">
+                <label for="email" class="label">Email</label><input class="input input-bordered" required type="email" name="email">
+                <label for="password" class="label">Password</label><input class="input input-bordered" required type="password" name="password">
+                <label for="password_confirmation" class="label">Password Confirm</label><input class="input input-bordered" required type="password" name="password_confirmation">
+                <div class="card-actions justify-center mt-2">
+                    <button class="btn btn-primary flex-grow" type="submit">Register</button>
+                    <x-googleauth />
+                </div>
+                <span>Already registered? <a class="link" href="/login">Login.</a></span>
+            </div>
+        </form>
+    </div>
 </x-layout>
