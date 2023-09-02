53bd38c727256b815bfd44b735d231a45cf9bdb2
Kostas Koukos <kkonst2014@gmail.com>
2023-09-02 15:48:56 +0300

    completed login page styling

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,16 +1,19 @@
 <x-layout title="Login">
 
-    <form action="/login" method="POST" class="card max-w-md h-1/3 m-auto">
-        <div class="card-body">
-            <h1 class="card-title">Login</h1>
-            @csrf
-            <label for="email" class="label">Email</label> <input class="input input-bordered" required type="email" name="email">
-            <label for="password" class="label">Password</label> <input class="input input-bordered" required type="password" name="password">
-            <div class="flex items-center gap-3"><label for="remember" class="label">Remember</label> <input class="toggle toggle-primary inline-flex" type="checkbox" name="remember"></div>
-            <div class="card-actions justify-center">
-                <button class="btn btn-primary flex-grow" type="submit">Login</button>
-                <x-googleauth />
+    <div class="flex h-screen items-center">
+        <form action="/login" method="POST" class="card m-auto w-screen md:w-1/3 md:bg-base-200">
+            <div class="card-body">
+                <h1 class="card-title">Login</h1>
+                @csrf
+                <label for="email" class="label">Email</label> <input class="input input-bordered" required type="email" name="email">
+                <label for="password" class="label">Password</label> <input class="input input-bordered" required type="password" name="password">
+                <div class="flex items-center gap-3"><label for="remember" class="label">Remember</label> <input class="toggle toggle-primary inline-flex" type="checkbox" name="remember"></div>
+                <div class="card-actions justify-center">
+                    <button class="btn btn-primary flex-grow" type="submit">Login</button>
+                    <x-googleauth />
+                </div>
+                <span>Don't have an account? <a class="link" href="/register">Make one here.</a></span>
             </div>
-        </div>
-    </form>
+        </form>
+    </div>
 </x-layout>
